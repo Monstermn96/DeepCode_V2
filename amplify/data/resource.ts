53ada2118adbb40a9c04fee59de438ada461d4d4
Eric Bischetsrieder <ericbischetsrieder@gmail.com>
2025-02-16 17:26:33 -0500

    removing bloat backend from template

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,7 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+  // Define your data models here when needed
 });
 
 export type Schema = ClientSchema<typeof schema>;
