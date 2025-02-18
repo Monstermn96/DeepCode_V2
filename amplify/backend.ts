7b2fddadd0d34a839f4c5ece653e3c2d043f9490
Eric Bischetsrieder <ericbischetsrieder@gmail.com>
2025-02-18 07:58:46 -0500

    stuff

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,10 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { userStats, tokenUsage, monthlyUsage } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { userStats, tokenUsage, monthlyUsage } from "./data/resource";
 
 export const backend = defineBackend({
-  // other resources
-  auth,
-
-  // each table is its own resource:
-  userStats,
-  tokenUsage,
-  monthlyUsage
+	auth,
+	userStats,
+	tokenUsage,
+	monthlyUsage,
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,61 +1,90 @@
-import { defineTable } from '@aws-amplify/cli-extensibility-helper';
+import { defineResource } from "@aws-amplify/cli-extensibility-helper";
 
-export const userStats = defineTable({
-  name: 'UserStats',
-  partitionKey: {
-    name: 'userId',
-    type: 'string'
-  },
-  stream: true,
-  ttl: {
-    attributeName: 'expiresAt',
-    enabled: false
-  }
+/**
+ * UserStats table
+ */
+export const userStats = defineResource(() => {
+	return {
+		Type: "AWS::DynamoDB::Table",
+		Properties: {
+			TableName: "UserStats",
+			KeySchema: [{ AttributeName: "userId", KeyType: "HASH" }],
+			AttributeDefinitions: [{ AttributeName: "userId", AttributeType: "S" }],
+			StreamSpecification: {
+				StreamViewType: "NEW_AND_OLD_IMAGES",
+			},
+			TimeToLiveSpecification: {
+				AttributeName: "expiresAt",
+				Enabled: false,
+			},
+			BillingMode: "PAY_PER_REQUEST",
+		},
+	};
 });
 
-export const tokenUsage = defineTable({
-  name: 'TokenUsage',
-  partitionKey: {
-    name: 'userId',
-    type: 'string'
-  },
-  sortKey: {
-    name: 'challengeId',
-    type: 'string'
-  },
-  stream: true,
-  ttl: {
-    attributeName: 'expiresAt',
-    enabled: true
-  },
-  globalSecondaryIndexes: [
-    {
-      indexName: 'byTimestamp',
-      partitionKey: {
-        name: 'userId',
-        type: 'string'
-      },
-      sortKey: {
-        name: 'timestamp',
-        type: 'string'
-      }
-    }
-  ]
+/**
+ * TokenUsage table
+ */
+export const tokenUsage = defineResource(() => {
+	return {
+		Type: "AWS::DynamoDB::Table",
+		Properties: {
+			TableName: "TokenUsage",
+			KeySchema: [
+				{ AttributeName: "userId", KeyType: "HASH" },
+				{ AttributeName: "challengeId", KeyType: "RANGE" },
+			],
+			AttributeDefinitions: [
+				{ AttributeName: "userId", AttributeType: "S" },
+				{ AttributeName: "challengeId", AttributeType: "S" },
+				{ AttributeName: "timestamp", AttributeType: "S" }, // for GSI
+			],
+			GlobalSecondaryIndexes: [
+				{
+					IndexName: "byTimestamp",
+					KeySchema: [
+						{ AttributeName: "userId", KeyType: "HASH" },
+						{ AttributeName: "timestamp", KeyType: "RANGE" },
+					],
+					Projection: { ProjectionType: "ALL" },
+				},
+			],
+			StreamSpecification: {
+				StreamViewType: "NEW_AND_OLD_IMAGES",
+			},
+			TimeToLiveSpecification: {
+				AttributeName: "expiresAt",
+				Enabled: true,
+			},
+			BillingMode: "PAY_PER_REQUEST",
+		},
+	};
 });
 
-export const monthlyUsage = defineTable({
-  name: 'MonthlyUsage',
-  partitionKey: {
-    name: 'userId',
-    type: 'string'
-  },
-  sortKey: {
-    name: 'yearMonth',
-    type: 'string'
-  },
-  stream: true,
-  ttl: {
-    attributeName: 'expiresAt',
-    enabled: true
-  }
+/**
+ * MonthlyUsage table
+ */
+export const monthlyUsage = defineResource(() => {
+	return {
+		Type: "AWS::DynamoDB::Table",
+		Properties: {
+			TableName: "MonthlyUsage",
+			KeySchema: [
+				{ AttributeName: "userId", KeyType: "HASH" },
+				{ AttributeName: "yearMonth", KeyType: "RANGE" },
+			],
+			AttributeDefinitions: [
+				{ AttributeName: "userId", AttributeType: "S" },
+				{ AttributeName: "yearMonth", AttributeType: "S" },
+			],
+			StreamSpecification: {
+				StreamViewType: "NEW_AND_OLD_IMAGES",
+			},
+			TimeToLiveSpecification: {
+				AttributeName: "expiresAt",
+				Enabled: true,
+			},
+			BillingMode: "PAY_PER_REQUEST",
+		},
+	};
 });
